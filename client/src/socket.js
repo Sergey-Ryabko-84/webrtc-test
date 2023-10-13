3c26dcc22d022c0a21a1993e40c1b5a2542da678
serhii.ryabko <serhii.ryabko@gmail.com>
2023-10-13 19:50:47 +0300

    change sockets

--- client/src/socket.js
+++ client/src/socket.js
@@ -1,4 +1,5 @@
-import io from 'socket.io-client';
+import io from "socket.io-client"
 // const sockets = io('http://localhost:3001', { autoConnect: true, forceNew: true });
-const sockets = io('/');
-export default sockets;
+// const sockets = io('/');
+const sockets = io(process.env.REACT_APP_BECKEND_URL)
+export default sockets
