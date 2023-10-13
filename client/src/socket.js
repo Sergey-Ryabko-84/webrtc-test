11aa3fc95c4727619f58ea2ea3333c957f397e02
serhii.ryabko <serhii.ryabko@gmail.com>
2023-10-13 19:56:02 +0300

    fix sockets

--- client/src/socket.js
+++ client/src/socket.js
@@ -1,5 +1,5 @@
 import io from "socket.io-client"
 // const sockets = io('http://localhost:3001', { autoConnect: true, forceNew: true });
 // const sockets = io('/');
-const sockets = io(process.env.REACT_APP_BECKEND_URL)
+const sockets = io(`${process.env.REACT_APP_BECKEND_URL}`)
 export default sockets
