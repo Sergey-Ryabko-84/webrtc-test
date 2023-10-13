e0ba5184fc90f9f135dddc7b58e2606233e9bbec
serhii.ryabko <serhii.ryabko@gmail.com>
2023-10-13 20:25:42 +0300

    change sockets

--- client/src/socket.js
+++ client/src/socket.js
@@ -1,5 +1,6 @@
 import io from "socket.io-client"
 // const sockets = io('http://localhost:3001', { autoConnect: true, forceNew: true });
-// const sockets = io('/');
-const sockets = io(`${process.env.REACT_APP_BECKEND_URL}`)
+// const sockets = io("/")
+const sockets = io("https://webrtc-test-10j5.onrender.com")
+
 export default sockets
